adfa71ab61001e0566c6148fc60832a66d02e968
Adam Tait <adam@adamtait.com>
2014-01-19 13:29:22 -0800

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Adam Tait on 1/19/14.
+//  Copyright (c) 2014 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TemperatureConverter
+//
+//  Created by Adam Tait on 1/19/14.
+//  Copyright (c) 2014 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
