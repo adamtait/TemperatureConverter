cc0228eb6b6da9bcccf7b06fea343a1228cc1b06
Adam Tait <adam@adamtait.com>
2014-01-19 22:23:24 -0800

    get temperature converter working. auto updating of opposite value.

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+- (IBAction)celsiusEditingChanged:(id)sender;
+- (IBAction)fahrenheitEditingChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *celsiusTextField;
+@property (weak, nonatomic) IBOutlet UITextField *fahrenheitTextField;
+
 @end
 
 @implementation ViewController
@@ -24,6 +29,24 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (float)convertCelsius:(float)temperatureInCelsius {
+    return ((temperatureInCelsius * 1.8) + 32);
+}
+
+- (float)convertFahrenheit:(float)temperatureInFahrenheit {
+    return ((temperatureInFahrenheit - 32) / 1.8);
+}
+
+- (IBAction)celsiusEditingChanged:(id)sender {
+    float newFahrenheitValue = [self convertCelsius:[self.celsiusTextField.text floatValue]];
+    self.fahrenheitTextField.text = [NSString stringWithFormat:@"%.0f", newFahrenheitValue];
+}
+
+- (IBAction)fahrenheitEditingChanged:(id)sender {
+    float newCelsiusValue = [self convertFahrenheit:[self.fahrenheitTextField.text floatValue]];
+    self.celsiusTextField.text = [NSString stringWithFormat:@"%.0f", newCelsiusValue];
 }
 
 @end
